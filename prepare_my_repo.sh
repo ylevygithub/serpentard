0fcb0376777a0dd37bd0e123a92aa1703546f204
yaacov <42210962+ylevygithub@users.noreply.github.com>
2019-05-14 16:52:06 +0200

    Delete preparemyrepo.sh

--- prepare_my_repo.sh
+++
@@ -1,4 +0,0 @@
-blih.py -u yaacov.levy@epitech.eu repository create $1
-blih.py -u yaacov.levy@epitech.eu repository setacl $1 ramassage-tek r
-blih.py -u yaacov.levy@epitech.eu repository getacl $1
-
